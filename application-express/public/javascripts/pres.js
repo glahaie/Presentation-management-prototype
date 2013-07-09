886a76f45f071652606c5fc64d1d9168da54af31
MaximeGir <maxime.g.girard@gmail.com>
2013-07-08 23:47:16 -0400

    fonctionnel sans controle

--- application-express/public/javascripts/pres.js
+++ application-express/public/javascripts/pres.js
@@ -1,28 +1,34 @@
-    // Créer les paramètres de la requête
-    
-    var ident = $('#presentationID').text(); 
+
+var ident = $('#presentationID').text(); 
     var ajaxObject = {
-        type: "Get",
+        type: "GET",
         url: "/presentation/" + ident,
         error: function(error) {
             if (error) {
                 console.log("Erreur");
             }
         },
-        success: function(response) {//mfp-hide
-            $('#saveEditor').html('<div id="tiny-pres" class="white-popup" >'+response+'</div>');
+        success: function(response) {
+		$('#saveEditor').html('<div id="tiny-pres" class="white-popup" >'+response+'</div>');
             console.log($('#test-popup').html());
         }
     };
     
     $.ajax(ajaxObject);
-  
-	$('#pres-button').click(
-	  function(){ 
-			$('.white-popup').removeAttr('id');
-			$('.white-popup').attr('id', 'test-popup');
-				$('.open-popup-link').magnificPopup({
-					type:'inline',
-					midClick: true 
-				});
-	         });
+     
+	$('.open-popup-link').magnificPopup({
+		type:'inline',
+		callbacks:{
+			beforeOpen: function() {
+                $('.white-popup').removeAttr('id');
+			    $('.white-popup').attr('id', 'test-popup');
+            },
+		    beforeClose: function(){
+				$('.white-popup').removeAttr('id');
+				$('.white-popup').attr('id', 'tiny-pres');
+			}
+		}
+	});
+
+
+			 
