ac4f99ccf4e811ef25c1a263c737a1a7498f3d3f
Marco <marco@cuthemustard.com>
2013-07-17 17:23:44 -0400

    presentation diaporama works ... sort of

--- application-express/public/javascripts/pres.js
+++ application-express/public/javascripts/pres.js
@@ -3,42 +3,28 @@ var iframe = ""
 $(document).ready(function(){
   $('#open-pres').click(function(){ transition();}); 
   /*setTimeout( function(){
-    iframe = frames[0].window.document;
-    $("body").on("keyup", $(iframe).defaultView, (function(e) {
-          if (e.keyCode == 27) { 
-           $('#saveEditor, #tiny-iframe').css({
-	         "width": "400px",
-            "min-width": "350px",
-            "position": "relative",
-            "height":"240px"
-	  });
-       }  
-    })); }, 2000);*/
-     
-    /*var ajaxObject = {
-        type: "GET",
-        url: "/presentation/presentation-demo",
-        error: function(error) {
-            if (error) {
-                console.log("Erreur");
-            }
-        },
-        success: function(response) {
-		$('#saveEditor').html(response);
-        iframeWindow = frames[0];
-	  
-	 }
-    };*/
-    
-    var transition = function(){
+    iframe = frames[0].window.document;*/
+  $(document).keyup( function(e) {
+    if (e.which == 27) {
+      $('#saveEditor, #tiny-iframe').css({
+        "width": "400px",
+        "min-width": "350px",
+        "position": "relative",
+        "height":"240px"
+       });
+     }
+     $('#bouton-fermer-diapo').hide();
+  });
+	     
+  var transition = function(){
       $('#saveEditor, #tiny-iframe').css(
 		    { "position": "absolute",
 			"top": "0",
 			"left": "0",
 			"width": "100%",
-			"height": "100%",
-			"display": "block"
+			"height": "100%"
 			});
+			$('#bouton-fermer-diapo').show();
     }
 	
    //$.ajax(ajaxObject);
