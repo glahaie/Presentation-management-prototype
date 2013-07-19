6117d84dff0f404c8729f8e0bd8f79cd35295c1c
Marco <marco@cuthemustard.com>
2013-07-19 12:23:13 -0400

    a little cleanup and fixed broken template

--- application-express/public/javascripts/pres.js
+++ application-express/public/javascripts/pres.js
@@ -10,30 +10,6 @@ $(document).ready(function(){
     }
   });
 	
-	var transitionBack = function() {
-	  $('#saveEditor, #tiny-iframe').css({
-      "width": "400px",
-      "min-width": "350px",
-      "position": "relative",
-      "height":"240px"
-    });
-    $("#bouton-fermer-diapo").remove();
-  }
-  
-  var transition = function(){
-      $('#saveEditor, #tiny-iframe').css(
-		    { "position": "absolute",
-			"top": "0",
-			"left": "0",
-			"width": "100%",
-			"height": "100%"
-			});
-			$('body').append($("<div id='bouton-fermer-diapo' style='display: block; position:absolute; right: 5%; top: 5%;'><a href='#'>x</a></div>" ));
-			$("#bouton-fermer-diapo").click( function(e) {
-			  transitionBack();
-			});
-    }
-	
 });
 
 $('#saveEditor').ready( function () {
@@ -53,3 +29,29 @@ $('#saveEditor').ready( function () {
   // ... $('#thumbnails-pages li').dblClick( .... )
   
 });
+
+function transitionBack() {
+  $('#saveEditor, #tiny-iframe').css({
+    "width": "400px",
+    "min-width": "350px",
+    "position": "relative",
+    "height":"240px"
+  });
+  $("#bouton-fermer-diapo").remove();
+}
+
+function transition() {
+  $('#saveEditor, #tiny-iframe').css({ 
+    "position": "absolute",
+	  "top": "0",
+	  "left": "0",
+	  "width": "100%",
+	  "height": "100%"
+	});
+	$('body').append($("<div id='bouton-fermer-diapo' style='display: block; position:absolute; right: 5%; top: 5%;'><a href='#'>x</a></div>" ));
+	$("#bouton-fermer-diapo").click( function(e) {
+	  transitionBack();
+	});
+}
+  
+  
